d2f8822103dfd641ec4a05237365e6c60e4a2369
chandrap2 <chandra2000@outlook.com>
2018-09-26 15:51:02 -0700

    Add files via upload

---
+++ surface_arduino.ino
@@ -0,0 +1,217 @@
+#include <XBOXUSB.h>
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#include <SPI.h>
+#endif
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+USB Usb;
+XBOXUSB Xbox(&Usb);
+EthernetUDP Udp;
+
+byte ip[]                                 = {10, 10, 5, 123};
+byte mac[]                             = {0x90, 0xa2, 0xda, 0x00, 0x11, 0x07};
+const unsigned int localPort  = 9631;
+byte        remoteIp[4] = {10, 10, 5, 124};
+unsigned int remotePort = 1369;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+const int joystickDeadzone = 8000;
+boolean button = false;
+//byte initBuffer[2] = {button, joystickDeadzone / 128.0};
+
+boolean firstTime = true;
+boolean sendStuff = false;
+
+byte last;
+boolean lastVert;
+boolean lastDir;
+boolean servoOn = false;
+
+byte numJoystickVals = 8;
+int rightx;
+int righty;
+int leftx;
+int lefty;
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1);
+  Ethernet.begin(mac, ip);
+  delay(1);
+  Udp.begin(localPort);
+  delay(1);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+
+  if (!button)
+  {
+#if !defined(__MIPSEL__)
+    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+
+    if (Usb.Init() == -1)
+    {
+      Serial.println(F("\r\nOSC did not start"));
+      while (1); //halt
+    }
+
+    Serial.println(F("\r\nXBOX USB Library Started..."));
+  }
+
+  else
+  {
+    lastVert = false;
+    lastDir = false;
+  }
+}
+
+void loop()
+{
+  byte stuff[80];
+  
+  if (firstTime)
+  {
+    stuff[0] = (byte) button;
+    stuff[1] = abs(joystickDeadzone) / 128.0;
+    
+    Udp.beginPacket(remoteIp, remotePort);
+    Udp.write(stuff, 80);
+    Udp.endPacket();
+
+    firstTime = false;
+  }
+  
+  else
+  {
+    int sizeOfPacket = Udp.parsePacket();
+    if (sizeOfPacket)
+    {
+      Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+  
+      Serial.println("*****received packet info*****");
+      //      Serial.println(sizeOfPacket);
+      ////      Serial.println(Udp.remoteIp());
+      ////      Serial.println(Udp.remotePort());
+      //
+      //      Serial.print("Contents: ");
+      //      Serial.println(packetBuffer);
+      //      Serial.println("************************************");
+      //      Serial.println();
+    }
+    
+    if (button)
+    {
+      if (lastVert != (boolean) digitalRead(2))
+      {
+        if (digitalRead(2) == HIGH) sendStuff = true;
+        servoOn = true;
+        lastVert = (boolean) digitalRead(2);
+      }
+  
+      else if (lastDir != (boolean) digitalRead(3))
+      {
+        if (digitalRead(3) == HIGH) sendStuff = true;
+        servoOn = false;
+        lastDir = (boolean) digitalRead(3);
+      }
+    } // if 'button' is true
+  
+    else
+    {
+      Usb.Task();
+  
+      if (Xbox.Xbox360Connected)
+      {
+        for (byte i = 0; i < 4 + numJoystickVals; i++)
+        {
+          stuff[i] = 0;
+        }
+  
+        rightx = constrain(Xbox.getAnalogHat(RightHatX), -32767, 32767);
+        righty = constrain(Xbox.getAnalogHat(RightHatY), -32767, 32767);
+        leftx = constrain(Xbox.getAnalogHat(LeftHatX), -32767, 32767);
+        lefty = constrain(Xbox.getAnalogHat(LeftHatY), -32767, 32767);
+  
+        if  (pow(pow(rightx, 2) + pow(righty, 2), 0.5) > joystickDeadzone)
+        {
+          stuff[1] = rightx > 0 ? 0 : 1;
+          stuff[0] = abs(rightx) / 128.0;
+  
+          stuff[3] = righty > 0 ? 0 : 1;
+          stuff[2] = abs(righty) / 128.0;
+  
+          sendStuff = true;
+          Serial.println("rightJS");
+        }
+  
+        if  (pow(pow(leftx, 2) + pow(lefty, 2), 0.5) > joystickDeadzone)
+        {
+          stuff[5] = leftx > 0 ? 0 : 1;
+          stuff[4] = abs(leftx) / 128.0;
+  
+          stuff[7] = lefty > 0 ? 0 : 1;
+          stuff[6] = abs(lefty) / 128.0;
+  
+          sendStuff = true;
+          Serial.println("leftJS");
+        }
+  
+        if (Xbox.getButtonClick(A))
+        {
+          stuff[numJoystickVals] = 0;
+          sendStuff = true;
+          Serial.println("buttonA");
+        }
+
+        else if (Xbox.getButtonClick(B))
+        {
+          stuff[numJoystickVals] = 1;
+          sendStuff = true;
+          Serial.println("buttonB");
+        }
+
+        else if (Xbox.getButtonClick(Y))
+        {
+          stuff[numJoystickVals] = 2;
+          sendStuff = true;
+          Serial.println("buttonY");
+        }
+
+        if (Xbox.getButtonClick(X))
+        {
+          stuff[numJoystickVals + 1] = 1;
+          sendStuff = true;
+          Serial.println("buttonX");
+        }
+      } // If the controller is connected
+      delay(1);
+    } // if 'button' is false
+  
+    if (sendStuff)
+    {
+      if (button)
+      {
+        Udp.beginPacket(remoteIp, remotePort);
+        Udp.write((byte) servoOn);
+        Udp.endPacket();
+      }
+  
+      else
+      {
+        Udp.beginPacket(remoteIp, remotePort);
+        Udp.write(stuff, 80);
+        Udp.endPacket();
+      }
+  
+      Serial.println("sent");
+      Serial.println();
+  
+      sendStuff = false;
+    } // if sendStuff
+  } // if not firstTime
+} // Loop function
+
---
+++ underwater_arduino.ino
@@ -0,0 +1,162 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <Servo.h>
+
+EthernetUDP Udp;
+
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
+Servo vertical;
+Servo motors[5] = {frontRight, frontLeft, backRight, backLeft, vertical};
+
+byte ip[]                    = {10, 10, 5, 124};
+byte mac[]                   = {0x90, 0xa2, 0xda, 0x00, 0x23, 0x16};
+const unsigned int localPort = 1369;
+byte        remoteIp[4] = {10, 10, 5, 123};
+unsigned int remotePort = 9631;
+char recvdBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+char replyBuffer[UDP_TX_PACKET_MAX_SIZE + 1];
+
+boolean button;
+boolean firstTime = true;
+
+int joystickDeadzone;
+
+int right[2]; // element one is x, two is y
+float *rightAngleMagPtr;
+float rightAngleMag[2]; // element one is angle. two is magnitude
+
+int left[2];
+float *leftAngleMagPtr;
+float leftAngleMag[2];
+
+byte gear; // 1 is A, 2 is B, 3 is Y
+boolean buttonX = false;
+
+int power = 0;
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+  analogWrite(4, HIGH);
+
+  for (int i = 0; i < 5; i++)
+  {
+    motors[i].attach(9 - i);
+    motors[i].writeMicroseconds(1500);
+  }
+
+  delay(1500);
+
+  Serial.begin(9600);
+  Ethernet.begin(mac, ip);
+  Udp.begin(localPort);
+  Serial.println("local init done");
+}
+
+void loop()
+{
+  rightAngleMag[0] = 0;
+  rightAngleMag[1] = 0;
+
+  leftAngleMag[0] = 0;
+  leftAngleMag[1] = 0;
+
+  int recvdSize = Udp.parsePacket(); // note that this includes the UDP header
+
+  if (recvdSize)
+  {
+    Udp.read(recvdBuffer, UDP_TX_PACKET_MAX_SIZE);
+
+    //    Serial.print("Received packet data size = ");
+    Serial.println(recvdSize);
+    //    Serial.print(" from ");
+    //    Serial.print(Udp.remoteIP());
+    //    Serial.print(", port number: ");
+    //    Serial.print(Udp.remotePort());
+    //    Serial.println();
+    //    Serial.println("Contents: ");
+
+    //    Serial.print("{");
+    //    for (int i = 0; i < recvdSize; i++)
+    //    {
+    //      Serial.print((byte) recvdBuffer[i]);
+    //      Serial.print(i != recvdSize - 1 ? ", " : "}");
+    //    }
+    //    Serial.println();
+
+    if (firstTime)
+    {
+      firstTime = !firstTime;
+      button = (recvdBuffer[0] == 1) ? true : false;
+      if (!button) joystickDeadzone = recvdBuffer[1];
+    }
+
+    else
+    {
+      if (button)
+      {
+        power = recvdBuffer[0];
+      }
+
+      else
+      {
+        right[0] = (byte) recvdBuffer[0] * ((byte) recvdBuffer[1] == 0 ? 1 : -1);
+        right[1] = (byte) recvdBuffer[2] * ((byte) recvdBuffer[3] == 0 ? 1 : -1);
+
+        left[0] = (byte) recvdBuffer[4] * ((byte) recvdBuffer[5] == 0 ? 1 : -1);
+        left[1] = (byte) recvdBuffer[6] * ((byte) recvdBuffer[7] == 0 ? 1 : -1);
+
+        rightAngleMagPtr = returnAngleMag(right[0], right[1]);
+        rightAngleMag[0] = *rightAngleMagPtr;
+        rightAngleMag[1] = *(rightAngleMagPtr + 1);
+
+        leftAngleMagPtr = returnAngleMag(left[0], left[1]);
+        leftAngleMag[0] = *leftAngleMagPtr;
+        leftAngleMag[1] = *(leftAngleMagPtr + 1);
+
+        gear = (byte) recvdBuffer[8];
+        buttonX = ((byte) recvdBuffer[9]) ? !buttonX : buttonX;
+      } // if not button
+    } // not firstTime
+  } // if something was recieved over ethernet
+
+  if (!firstTime)
+  {
+    if (button)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        motors[i].writeMicroseconds(power * -1 * 200 + 1500);
+      }
+    }
+
+    else
+    {
+      float sine = sin(PI * ((rightAngleMag[0]) / 180 - .25)) * rightAngleMag[1] / (255.0 - joystickDeadzone);
+      float cosine = cos(PI * ((rightAngleMag[0] / 180) - .25)) * rightAngleMag[1] / (255.0 - joystickDeadzone);
+      
+      motors[0].writeMicroseconds(1500 + (400 * gear) + (400.0 / 3 * sine));
+      motors[1].writeMicroseconds(1500 + (400 * gear) + (400.0 / 3 * cosine));
+      motors[2].writeMicroseconds(1500 - (400 * gear) - (400.0 / 3 * cosine));
+      motors[3].writeMicroseconds(1500 - (400 * gear) - (400.0 / 3 * sine));
+      
+      motors[4].writeMicroseconds(1500 + 400 * (sin(leftAngleMag[0] / 180 * PI) * leftAngleMag[1] / (255.0 - joystickDeadzone)));
+    } // if not button
+  } // if not firstTime (send values to thrusters)
+} // Loop function
+
+float *returnAngleMag(float x, float y)
+{
+  static float angleMag[2];
+
+  angleMag[0] = atan2(y, x) / PI * 180.0;
+  angleMag[0] += (angleMag[0] > 0) ? 0 : 360;
+  angleMag[1] = pow(pow(x, 2) + pow(y, 2), 0.5) - joystickDeadzone;
+
+  return angleMag;
+}
+
